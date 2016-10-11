44e24fd752275a6c3649065a8fea7de427de7a67
ekoetsier <ernst.koetsier@sicpa.com>
2016-10-11 16:27:37 -0300

    Aula 6 final

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -21,10 +21,14 @@ import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -35,6 +39,10 @@ import com.google.common.cache.CacheBuilder;
 @ComponentScan( basePackages= {"br.com.casadocodigo.loja" })
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LocaleChangeInterceptor());
+	}
 	
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -42,6 +50,11 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/resources/images/**").addResourceLocations("/resources/imagens/").setCachePeriod(31556926);
         registry.addResourceHandler("/resources/js/**").addResourceLocations("/resources/js/").setCachePeriod(31556926);
     }
+	
+	@Bean
+	public LocaleResolver localeResolver(){
+	    return new CookieLocaleResolver();
+	}
 
 	@Bean
 	public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager manager){
